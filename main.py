f3057e99cb1780a2cbc35a2569805f5e79eea582
Afaz <158006345+Afaz-Developer@users.noreply.github.com>
2024-07-03 18:02:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from pytube import YouTube
 from pydub import AudioSegment
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from starlette.responses import FileResponse
 import aiofiles
+import requests
 
 app = FastAPI()
 
@@ -47,10 +48,33 @@ async def download_youtube_video(url, output_path='downloads'):
         print(f"An error occurred: {e}")
         return None
 
+def remove_files_in_directory(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print(f"Deleted {file_path}")
+        except Exception as e:
+            print(f"Failed to delete {file_path}: {e}")
+
+# Server management stuff ===
 @app.get("/")
 def read_root():
     return "/audio/m/{code} for audio, /video/m/{code} for video"
 
+@app.get("/deploy")
+def deploy():
+    requests.get('https://api.render.com/deploy/srv-cq2hm2t6l47c73b626a0?key=BOH_-qChfhw')
+    return "Deployed!"
+
+@app.get("/reset")
+def reset():
+    remove_files_in_directory('downloads')
+    return "Removed all downloaded files!"
+
+
+# Downloadings ===
 @app.get("/audio/m/{url}")
 async def download_audio(url: str):
     file_path = await download_youtube_audio("https://m.youtube.com/watch?v=" + url)
@@ -62,6 +86,22 @@ async def download_audio(url: str):
 @app.get("/video/m/{url}")
 async def download_video(url: str):
     file_path = await download_youtube_video("https://m.youtube.com/watch?v=" + url)
+    if file_path:
+        return FileResponse(file_path, media_type='video/mp4', filename=os.path.basename(file_path))
+    else:
+        raise HTTPException(status_code=500, detail="Error downloading video")
+
+@app.get("/watch/")
+async def watch_video(v: str = Query(..., alias='v')):
+    file_path = await download_youtube_video("https://m.youtube.com/watch?v=" + v)
+    if file_path:
+        return FileResponse(file_path, media_type='video/mp4', filename=os.path.basename(file_path))
+    else:
+        raise HTTPException(status_code=500, detail="Error downloading video")
+
+@app.get("/audio/watch/")
+async def listen_audio(v: str = Query(..., alias='v')):
+    file_path = await download_youtube_video("https://m.youtube.com/watch?v=" + v)
     if file_path:
         return FileResponse(file_path, media_type='video/mp4', filename=os.path.basename(file_path))
     else:
